359cc1061cce20b0af6ce45441af9c573e740d63
Gabriela <magabriela0715@gmai.com>
2022-12-04 11:23:06 -0600

    changes in firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+//import { getFirestore } from "firebase/firestore";
 import { getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBON8SraDq6WxiBU6I8F_q6fcXg3UeKsTQ",
-  authDomain: "adastrahub-app.firebaseapp.com",
-  projectId: "adastrahub-app",
-  storageBucket: "adastrahub-app.appspot.com",
-  messagingSenderId: "432707335665",
-  appId: "1:432707335665:web:547e95a757dd1499988050",
-  measurementId: "G-81LJXWXM91"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID ,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
